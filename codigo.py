9f3755f45cfcc79769fe51587e1803a55ed1a26d
Camilo Andrés Domínguez <91099532+camilodguez@users.noreply.github.com>
2024-08-16 14:51:44 -0500

    Update AGREGRANDO BMW codigo.py

--- codigo.py
+++ codigo.py
@@ -1,4 +1,4 @@
 nombre = "Juan Pablo"
-apellido = "Montolla"
+apellido = "Montolla BMW"
 print(f"Bienvendio {nombre} {apellido} al curso de Dalto en git")
 print("Pongase comodo y disfrute del video!")
